d5a1969bc46d0bd446b4bd3b6cfd5de58f051841
Jan Frömberg <jan.froemberg@tu-dresden.de>
2018-06-18 15:02:51 +0200

    wrong setting variable fixed

--- hcc_py/settings.py
+++ hcc_py/settings.py
@@ -56,7 +56,7 @@ LOGGING = {
 }
 
 # Setup support for proxy headers
-FORWARDED_HOST = True
+USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
