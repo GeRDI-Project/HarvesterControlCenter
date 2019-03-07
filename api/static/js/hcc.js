6160e9d33103dcc8af1c89f165cf246d47627962
Jan Frömberg <jan.froemberg@tu-dresden.de>
2019-03-07 17:54:56 +0100

    MDM-53 remoev deprecated jquery document ready call

--- api/static/js/hcc.js
+++ api/static/js/hcc.js
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
  */
 
-$(document).ready(function () {
+$( function () {
     
     $('#loaderSpinnerLog').hide();
     $('#loaderSpinnerStat').hide();
