f7b63bfe6ef5817128da05e9b8d595a4135d6700
Jan Frömberg <jan.froemberg@tu-dresden.de>
2019-04-15 12:04:09 +0200

    MDM-65 header will disappear when scroll down

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Harvester Control Center (HCC) v3.6.2
+# Harvester Control Center (HCC) v3.7.2
 
 A Harvester Control Center GUI with REST-API written in Django.
 
--- api/static/js/hcc.js
+++ api/static/js/hcc.js
@@ -334,3 +334,15 @@ function filterFunction() {
       }
     }
 }
+
+$( window ).scroll(function(e) {
+
+    // add/remove class to navbar when scrolling to hide/show
+    var scroll = $(window).scrollTop();
+    if (scroll >= 150) {
+        $('.navbar').addClass("navbar-hide");
+    } else {
+        $('.navbar').removeClass("navbar-hide");
+    }
+
+});
