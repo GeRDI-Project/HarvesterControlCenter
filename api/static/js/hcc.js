ae8735f0c43477c69822fcfb3f59ad3f1ff1bc3f
Jan Frömberg <jan.froemberg@tu-dresden.de>
2019-04-15 15:15:46 +0200

    MDM-65 improved header

--- api/static/js/hcc.js
+++ api/static/js/hcc.js
@@ -339,7 +339,7 @@ $( window ).scroll(function(e) {
 
     // add/remove class to navbar when scrolling to hide/show
     var scroll = $(window).scrollTop();
-    if (scroll >= 150) {
+    if (scroll >= 270) {
         $('.navbar').addClass("navbar-hide");
     } else {
         $('.navbar').removeClass("navbar-hide");
