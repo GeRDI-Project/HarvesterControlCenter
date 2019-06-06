50e704b4bd57b42a901a62db13a68d66db546f89
Jan Frömberg <jan.froemberg@tu-dresden.de>
2019-06-06 14:07:19 +0200

    MDM-63 changed from basic auth to session auth to prevent "relogin"

--- hcc_py/settings.py
+++ hcc_py/settings.py
@@ -144,7 +144,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
