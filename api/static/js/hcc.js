db423689ffa00fd9d70480e464a55228cc9a80f6
Laura Hoehle <laura.hoehle@mailbox.tu-dresden.de>
2019-08-20 15:17:47 +0200

    MDM-63 some code cleaning in hcc.js

--- api/static/js/hcc.js
+++ api/static/js/hcc.js
@@ -529,22 +529,25 @@ function checkboxHideIdle() {
 
 //implement own add/remove class methods to avoid jquery
 function hasClass(el, className) {
-    if (el.classList)
+    if (el.classList) {
         return el.classList.contains(className);
-    else
+    } else {
         return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+    }
 }
 
 function addClass(el, className) {
-    if (el.classList)
+    if (el.classList) {
         el.classList.add(className);
-    else if (!hasClass(el, className)) el.className += " " + className;
+    } else if (!hasClass(el, className)) {
+        el.className += " " + className;
+    }
 }
 
 function removeClass(el, className) {
-    if (el.classList)
+    if (el.classList) {
         el.classList.remove(className);
-    else if (hasClass(el, className)) {
+    } else if (hasClass(el, className)) {
         var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
         el.className = el.className.replace(reg, ' ');
     }
