f8a8184c0fa952cc60b1ff5cefca496f530a6a73
Jan Frömberg <jan.froemberg@tu-dresden.de>
2019-08-27 13:28:50 +0200

    bumped version to reflect added features

--- hcc_py/settings.py
+++ hcc_py/settings.py
@@ -18,7 +18,7 @@ __author__ = "Jan Frömberg"
 __copyright__ = "Copyright 2018, GeRDI Project"
 __credits__ = ["Jan Frömberg"]
 __license__ = "Apache 2.0"
-__version__ = "3.9.0"
+__version__ = "3.10.0"
 __maintainer__ = "Jan Frömberg"
 __email__ = "jan.froemberg@tu-dresden.de"
 
