3f0f36cdcadc58a3ef581bbda5112c4e8ac11f51
Laura Hoehle <laura.hoehle@mailbox.tu-dresden.de>
2019-12-19 11:02:48 +0100

    MDM-92 the issue was caused by the new version of djangorestframework (3.10.3). Had to add a new line in settings.py to make it work again.

--- hcc_py/settings.py
+++ hcc_py/settings.py
@@ -153,6 +153,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'PAGE_SIZE': 10
 }
 
