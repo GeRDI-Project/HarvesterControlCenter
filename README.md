cec6d5868812e69e62ea8abe7f797916c2fb6679
Jan Frömberg <jan.froemberg@tu-dresden.de>
2019-12-19 13:37:43 +0100

    MDM-87 version bumped

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Harvester Control Center (HCC) v3.12.0
+# Harvester Control Center (HCC) v3.16.0
 
 A Harvester Control Center GUI with REST-API written in Django.
 
--- hcc_py/settings.py
+++ hcc_py/settings.py
@@ -18,7 +18,7 @@ __author__ = "Jan Frömberg"
 __copyright__ = "Copyright 2018, GeRDI Project"
 __credits__ = ["Jan Frömberg"]
 __license__ = "Apache 2.0"
-__version__ = "3.12.0"
+__version__ = "3.16.0"
 __maintainer__ = "Jan Frömberg"
 __email__ = "jan.froemberg@tu-dresden.de"
 
